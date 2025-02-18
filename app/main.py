1f1c4a6f351aaca0af9629cca1e04401cf2b7938
dariuszlenart <dariusz@lenart-it.pl>
2025-02-18 14:20:56 +0100

    fix: CORS

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from redis.asyncio import Redis
+from starlette.middleware.cors import CORSMiddleware
+
+from app.routers import ws
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -9,6 +13,14 @@ async def lifespan(app: FastAPI):
     await app.state.redis.close()
 
 app = FastAPI(lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Tighten this in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.include_router(ws.router, prefix="/ws")
 
 @app.get("/health")
 async def health_check():
